302e94a5d038819d1849ede7028033528258470c
Roberto Breve <rbreve@gmail.com>
2019-11-12 17:01:44 +0200

    Initial Commit

---
+++ Pagination/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pagination
+//
+//  Created by Roberto Breve  on 12.11.2019.
+//  Copyright © 2019 Roberto Breve . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
